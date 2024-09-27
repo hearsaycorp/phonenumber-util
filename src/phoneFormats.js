7c2cb3d2cc33cd764530f5424f67a2fd3f91d4a4
Brian J <bajohnson@yext.com>
2024-09-27 08:44:49 -0700

    Initial commit

---
+++ src/phoneFormats.js
@@ -0,0 +1,212 @@
+export const PHONE_FORMATS = {
+  1: '(xxx) xxx-xxxx', // United States
+  7: '+x xxx xxx-xx-xx', // Russia
+  20: '+xx x xxxxxxxx', // Egypt
+  27: '+xx xx xxx xxxx', // South Africa
+  30: '+xx xxx xxx xxxx', // Greece
+  31: '+xx xx xxx xxxx', // Netherlands
+  32: '+xx xxx xx xx xx', // Belgium
+  33: '+xx x xx xx xx xx', // France
+  34: '+xx xxx xx xx xx', // Spain
+  36: '+xx xx xxx xxxx', // Hungary
+  39: '+xx xxx xxx xxxx', // Italy
+  40: '+xx xxx xxx xxxx', // Romania
+  41: '+xx xx xxx xx xx', // Switzerland
+  43: '+xx xxx xxx xxxx', // Austria
+  44: '+xx xxxx xxxxxx', // United Kingdom
+  45: '+xx xx xx xx xx', // Denmark
+  46: '+xx xx xxx xx xx', // Sweden
+  47: '+xx xxx xx xxx', // Norway
+  48: '+xx xxx xxx xxx', // Poland
+  49: [
+    '+xx xx xxxxxx',
+    '+xx xx xxxxxxx',
+    '+xx xx xxxxxxxx',
+    '+xx xxx xxxxxxxx',
+  ], // Germany
+  51: '+xx xxx xxx xxx', // Peru
+  52: '+xx x xxxx xxxx', // Mexico
+  53: '+x xxx xxxx', // Cuba
+  54: '+xx xx xxxx xxxx', // Argentina
+  55: '+xx xx xxxx xxxx', // Brazil
+  56: '+xx x xxxx xxxx', // Chile
+  57: '+xx xxx xxx xxxx', // Colombia
+  58: '+xx xxx xxx xxxx', // Venezuela
+  60: '+xx xx xxx xxxx', // Malaysia
+  61: '+xx xxxx xxxx', // Australia
+  62: '+xx xxx xxx xxx', // Indonesia
+  63: '+xx xxx xxx xxxx', // Philippines
+  64: '+xx xxx xxx xxxx', // New Zealand
+  65: '+xx xxxx xxxx', // Singapore
+  66: '+xx xx xxx xxxx', // Thailand
+  81: '+xx xx xxxx xxxx', // Japan
+  82: '+xx xx xxx xxxx', // South Korea
+  84: '+xx xxx xxxx xxxx', // Vietnam
+  86: '+xx xxx xxxx xxxx', // China
+  90: '+xx xxx xxx xxxx', // Turkey
+  91: '+xx xx xxxx xxxx', // India
+  92: '+xx xxx xxx xxxx', // Pakistan
+  93: '+xx xx xxx xxxx', // Afghanistan
+  94: '+xx xx xxx xxxx', // Sri Lanka
+  95: '+xx xxx xxx xxxx', // Myanmar
+  98: '+xx xxx xxx xxxx', // Iran
+  211: '+xxx xxx xxxx', // South Sudan
+  212: '+xxx xx xx xx xx', // Morocco
+  213: '+xxx xx xx xx', // Algeria
+  216: '+xxx xx xxx', // Tunisia
+  218: '+xxx xx xxx xxx', // Libya
+  220: '+xxx xxx xxxx', // Gambia
+  221: '+xxx xxx xxx xxx', // Senegal
+  222: '+xxx xxx xxxx', // Mauritania
+  223: '+xxx xxx xxxx', // Mali
+  224: '+xxx xxx xxx xxx', // Guinea
+  225: '+xxx xx xx xx xx', // Ivory Coast
+  226: '+xxx xx xx xx xx', // Burkina Faso
+  227: '+xxx xx xx xx xx', // Niger
+  228: '+xxx xxx xxx xxx', // Togo
+  229: '+xxx xx xx xx xx', // Benin
+  230: '+xxx xxxx xxxx', // Mauritius
+  231: '+xxx xx xx xx xx', // Liberia
+  232: '+xxx xxx xxxx', // Sierra Leone
+  233: '+xxx xxx xxxx', // Ghana
+  234: '+xxx xxx xxxx xxxx', // Nigeria
+  235: '+xxx xx xx xx xx', // Chad
+  236: '+xxx xx xx xx xx', // Central African Republic
+  237: '+xxx xxxx xxxx', // Cameroon
+  238: '+xxx xxxx xxxx', // Cape Verde
+  239: '+xxx xxxx xxxx', // Sao Tome and Principe
+  240: '+xxx xxx xxxx xxxx', // Equatorial Guinea
+  241: '+xxx xxx xxxx xxxx', // Gabon
+  242: '+xxx xxx xxxx xxxx', // Republic of the Congo
+  243: '+xxx xxx xxxx xxxx', // Democratic Republic of the Congo
+  244: '+xxx xxx xxxx xxxx', // Angola
+  245: '+xxx xxx xxxx xxxx', // Guinea-Bissau
+  246: '+xxx xxx xxxx', // British Indian Ocean Territory
+  248: '+xxx xxxx xxxx', // Seychelles
+  249: '+xxx xxx xxxx xxxx', // Sudan
+  250: '+xxx xxx xxxx xxxx', // Rwanda
+  251: '+xxx xxx xxxx xxxx', // Ethiopia
+  252: '+xxx xxx xxxx xxxx', // Somalia
+  253: '+xxx xxx xxxx xxxx', // Djibouti
+  254: '+xxx xxx xxxx xxxx', // Kenya
+  255: '+xxx xxx xxxx xxxx', // Tanzania
+  256: '+xxx xxx xxxx xxxx', // Uganda
+  257: '+xxx xx xx xx xx', // Burundi
+  258: '+xxx xxx xxxx xxxx', // Mozambique
+  260: '+xxx xxx xxxx xxxx', // Zambia
+  261: '+xxx xx xx xx xx', // Madagascar
+  263: '+xxx xxx xxxx xxxx', // Zimbabwe
+  264: '+xxx xxx xxxx xxxx', // Namibia
+  265: '+xxx xxx xxxx xxxx', // Malawi
+  266: '+xxx xx xx xx xx', // Lesotho
+  267: '+xxx xxx xxxx xxxx', // Botswana
+  268: '+xxx xx xx xx xx', // Eswatini
+  269: '+xxx xx xx xx xx', // Comoros
+  290: '+xxx xxxx xxxx', // Saint Helena
+  291: '+xxx xxx xxxx xxxx', // Eritrea
+  297: '+xxx xxxx xxxx', // Aruba
+  298: '+xxx xxx xxx', // Faroe Islands
+  299: '+xxx xxx xxx xxx', // Greenland
+  350: '+xxx xxx xxxx xxxx', // Gibraltar
+  351: '+xxx xxx xxxx xxxx', // Portugal
+  352: '+xxx xxx xxxx xxxx', // Luxembourg
+  353: '+xxx xxx xxxx xxxx', // Ireland
+  354: '+xxx xxx xxxx xxxx', // Iceland
+  355: '+xxx xxx xxxx xxxx', // Albania
+  356: '+xxx xx xx xx xx', // Malta
+  357: '+xxx xxx xxxx xxxx', // Cyprus
+  358: '+xxx xxx xxxx xxxx', // Finland
+  359: '+xxx xxx xxxx xxxx', // Bulgaria
+  370: '+xxx xxx xxxx xxxx', // Lithuania
+  371: '+xxx xxx xxxx xxxx', // Latvia
+  372: '+xxx xxx xxxx xxxx', // Estonia
+  373: '+xxx xxx xxxx xxxx', // Moldova
+  374: '+xxx xxx xxxx xxxx', // Armenia
+  375: '+xxx xxx xxxx xxxx', // Belarus
+  376: '+xxx xxx xxxx xxxx', // Andorra
+  377: '+xxx xxx xxxx xxxx', // Monaco
+  378: '+xxx xxx xxxx xxxx', // San Marino
+  379: '+xxx xxx xxxx xxxx', // Vatican
+  380: '+xxx xxx xxxx xxxx', // Ukraine
+  381: '+xxx xxx xxxx xxxx', // Serbia
+  382: '+xxx xxx xxxx xxxx', // Montenegro
+  383: '+xxx xxx xxxx xxxx', // Kosovo
+  385: '+xxx xxx xxxx xxxx', // Croatia
+  386: '+xxx xxx xxxx xxxx', // Slovenia
+  387: '+xxx xxx xxxx xxxx', // Bosnia and Herzegovina
+  389: '+xxx xxx xxxx xxxx', // North Macedonia
+  420: '+xxx xxx xxxx xxxx', // Czech Republic
+  421: '+xxx xxx xxxx xxxx', // Slovakia
+  423: '+xxx xxxx xxxx xxxx', // Liechtenstein
+  500: '+xxx xxxx xxxx', // Falkland Islands
+  501: '+xxx xxxx xxxx', // Belize
+  502: '+xxx xxxx xxxx', // Guatemala
+  503: '+xxx xxxx xxxx', // El Salvador
+  504: '+xxx xxxx xxxx', // Honduras
+  505: '+xxx xxxx xxxx', // Nicaragua
+  506: '+xxx xxxx xxxx', // Costa Rica
+  507: '+xxx xxxx xxxx', // Panama
+  508: '+xxx xxxx xxxx', // Saint Pierre and Miquelon
+  509: '+xxx xxxx xxxx', // Haiti
+  590: '+xxx xxxx xxxx', // Saint Martin
+  591: '+xxx xxx xxxx xxxx', // Bolivia
+  592: '+xxx xxxx xxxx', // Guyana
+  593: '+xxx xxx xxxx xxxx', // Ecuador
+  594: '+xxx xxxx xxxx', // French Guiana
+  595: '+xxx xxx xxxx xxxx', // Paraguay
+  596: '+xxx xxxx xxxx', // Martinique
+  597: '+xxx xxxx xxxx', // Suriname
+  598: '+xxx xxx xxxx xxxx', // Uruguay
+  599: '+xxx xxxx xxxx xxxx', // Curaçao
+  670: '+xxx xxxx xxxx', // East Timor
+  672: '+xxx xxxx xxxx', // Antarctica
+  673: '+xxx xxxx xxxx', // Brunei
+  674: '+xxx xxxx xxxx', // Nauru
+  675: '+xxx xxxx xxxx xxxx', // Papua New Guinea
+  676: '+xxx xxxx xxxx xxxx', // Tonga
+  677: '+xxx xxxx xxxx xxxx', // Solomon Islands
+  678: '+xxx xxxx xxxx xxxx', // Vanuatu
+  679: '+xxx xxxx xxxx xxxx', // Fiji
+  680: '+xxx xxxx xxxx xxxx', // Palau
+  681: '+xxx xxxx xxxx xxxx', // Wallis and Futuna
+  682: '+xxx xxxx xxxx xxxx', // Cook Islands
+  683: '+xxx xxxx xxxx xxxx', // Niue
+  685: '+xxx xxxx xxxx xxxx', // Samoa
+  686: '+xxx xxxx xxxx xxxx', // Kiribati
+  687: '+xxx xxxx xxxx xxxx', // New Caledonia
+  688: '+xxx xxxx xxxx xxxx', // Tuvalu
+  689: '+xxx xxxx xxxx xxxx', // French Polynesia
+  690: '+xxx xxxx xxxx xxxx', // Tokelau
+  691: '+xxx xxxx xxxx xxxx', // Micronesia
+  692: '+xxx xxxx xxxx xxxx', // Marshall Islands
+  850: '+xxx xxxx xxxx xxxx', // North Korea
+  852: '+xxx xxxx xxxx xxxx', // Hong Kong
+  853: '+xxx xxxx xxxx xxxx', // Macau
+  855: '+xxx xxx xxxx xxxx', // Cambodia
+  856: '+xxx xxxx xxxx xxxx', // Laos
+  880: '+xxx xxx xxxx xxxx', // Bangladesh
+  886: '+xxx xxxx xxxx xxxx', // Taiwan
+  960: '+xxx xxx xxxx xxxx', // Maldives
+  961: '+xxx xxx xxxx xxxx', // Lebanon
+  962: '+xxx xxxx xxxx xxxx', // Jordan
+  963: '+xxx xxxx xxxx xxxx', // Syria
+  964: '+xxx xxxx xxxx xxxx', // Iraq
+  965: '+xxx xxxx xxxx xxxx', // Kuwait
+  966: '+xxx xxxx xxxx xxxx', // Saudi Arabia
+  967: '+xxx xxxx xxxx xxxx', // Yemen
+  968: '+xxx xxxx xxxx xxxx', // Oman
+  970: '+xxx xxx xxxx xxxx', // Palestine
+  971: '+xxx xxx xxxx xxxx', // United Arab Emirates
+  972: '+xxx xxxx xxxx xxxx', // Israel
+  973: '+xxx xxxx xxxx xxxx', // Bahrain
+  974: '+xxx xxxx xxxx xxxx', // Qatar
+  975: '+xxx xxxx xxxx xxxx', // Bhutan
+  976: '+xxx xxx xxxx xxxx', // Mongolia
+  977: '+xxx xxxx xxxx xxxx', // Nepal
+  992: '+xxx xxxx xxxx xxxx', // Tajikistan
+  993: '+xxx xxx xxxx xxxx', // Turkmenistan
+  994: '+xxx xxx xxxx xxxx', // Azerbaijan
+  995: '+xxx xxxx xxxx xxxx', // Georgia
+  996: '+xxx xxxx xxxx xxxx', // Kyrgyzstan
+  998: '+xxx xxxx xxxx xxxx', // Uzbekistan
+};
