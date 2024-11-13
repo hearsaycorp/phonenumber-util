d155bb0caf2f5c4e3f86a15f517b979fac60b729
Brian J <bajohnson@yext.com>
2024-11-13 09:24:59 -0800

    61 moved to just Aus

--- src/phoneCodes.js
+++ src/phoneCodes.js
@@ -492,11 +492,7 @@ export const REGION_CODES = {
   235: { name: 'Chad', code: 'TD', flag: '🇹🇩' },
   56: { name: 'Chile', code: 'CL', flag: '🇨🇱' },
   86: { name: 'China', code: 'CN', flag: '🇨🇳' },
-  61: {
-    name: 'Australia, Christmas Island, Cocos Islands',
-    code: 'AU/CX',
-    flag: '🇦🇺/🇨🇽',
-  },
+  61: { name: 'Australia', code: 'AU', flag: '🇦🇺' },
   57: { name: 'Colombia', code: 'CO', flag: '🇨🇴' },
   269: { name: 'Comoros', code: 'KM', flag: '🇰🇲' },
   682: { name: 'Cook Islands', code: 'CK', flag: '🇨🇰' },
